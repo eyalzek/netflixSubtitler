f2fe6f40514342ec77ff858c44876ecb40164eed
eyalzek <eyal.ez@gmail.com>
2014-01-21 15:59:35 +0200

    second commit

--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function () {
-  chrome.tabs.getSelected(null,function(tab) {
-    var tablink = tab.url;
+	chrome.tabs.getSelected(null,function(tab) {
+	var tablink = tab.url;
+	var b = decodeURIComponent(tablink);
+	b = b.split("&t=")[1];
+	b = b.split("+");
+	var c = "";
+	for (i = 0; i < b.length; i++) {
+		c += b[i].match(/\w+/) + " ";
+	}
+	document.querySelector('.container').innerHTML = c;
 });
 });
